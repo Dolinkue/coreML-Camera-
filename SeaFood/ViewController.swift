c4ed578fd130ecedc85c5c15c40c9c1da2636946
dolinkue <dolinkue_n@me.com>
2022-04-10 12:11:56 -0300

    configurando câmara y album

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -23,10 +23,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagepicker.delegate = self
         // con esto llamamos a la camara para que la pueda usar el usuario
         imagepicker.sourceType = .camera
-        
+        imagepicker.allowsEditing = false
     }
 
+    // agregamos delegate method para cuando el user elija la foto la pasa a la ML, viene de UImagepickercontroller. es decir elegimos el metodo que le avisa a la app que ya eligio la foto y que hacer con ella
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+      if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagepicker.dismiss(animated: true)
+        
+    }
+    
+    
+    
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagepicker, animated: true)
+        
+        
     }
     
 }
